9fbdd09ba8810bdb4a3a0ccf979f986db1e48d06
aboodalda <47452876+aboodalda@users.noreply.github.com>
2026-09-21 09:54:04 +0300

    Add Firebase web configuration

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDtYSLUMMPovbUBJfDtLfMs6TESUm_xmAA",
+  authDomain: "al-wajha-menu.firebaseapp.com",
+  projectId: "al-wajha-menu",
+  storageBucket: "al-wajha-menu.firebasestorage.app",
+  messagingSenderId: "796184421337",
+  appId: "1:796184421337:web:f7185bca50533d5fd3afc8"
+};
